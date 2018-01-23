69b65a620755798895270c07e2a43ee88c746cd2
Shlomi Groner <salamigoner@gmail.com>
2018-01-23 22:01:40 +0200

    shlomi: app update

--- app/src/main/java/com/ihome/android/ihome/Upload_Service.java
+++ app/src/main/java/com/ihome/android/ihome/Upload_Service.java
@@ -1,9 +1,25 @@
 package com.ihome.android.ihome;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class Upload_Service extends IntentService {
@@ -14,7 +30,121 @@ public class Upload_Service extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Toast toast = Toast.makeText(getApplicationContext(), intent.getDataString(), Toast.LENGTH_LONG);
-        toast.show();
+        try{
+
+            Socket soc = new Socket(getString(R.string.SERVER_IP), 1618); // open socket
+            PrintWriter writer = new PrintWriter(soc.getOutputStream());
+            String fileNames = "";
+            String[] parse;
+            String ans;
+            FileInputStream fileinputstream;
+            BufferedInputStream bufferinputstream = null;
+            OutputStream outputstream = null;
+
+            //send filenames
+            String[] filePaths = intent.getData().toString().split("&&");
+            for (int i = 0; i < filePaths.length; i++)
+            {
+                parse = filePaths[i].split("/");
+                fileNames = fileNames + parse[parse.length - 1] + "&&";
+            }
+
+
+            fileNames = fileNames.substring(0, fileNames.length()-2);
+            // Send Upload code according to protocol to server
+            writer.write("103" + "@@" + "NEW_TXT.txt"); // works when we send HELLO
+            writer.flush();
+
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            final StringBuilder response = new StringBuilder();
+            String line;
+            //read from socket
+            while ((line = reader.readLine()) != null)
+                response.append(line);
+
+            /*Log.d("testing", response.toString());
+
+
+            writer.write("blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"); // works when we send HELLO
+            writer.flush();
+
+            while ((line = reader.readLine()) != null)
+                response.append(line);
+
+            Log.d("testing", response.toString());
+
+            writer.close();*/
+            //check if server approves
+            if(!response.toString().equals("200"))
+            {
+                Handler handler = new Handler(Looper.getMainLooper());//this is how to do "Toast" in service
+
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(Upload_Service.this.getApplicationContext(),"Server Didn't Received File Names",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                writer.close();
+            }
+            else
+            {
+                Handler handler = new Handler(Looper.getMainLooper()); //this is how to do "Toast" in service
+
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(Upload_Service.this.getApplicationContext(),response.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                for (int i = 0; i < filePaths.length; i++)
+                {
+                    // Send file to server
+                    try {
+                        // send file
+                        File file = new File(filePaths[i]);
+                        byte [] byteArray  = new byte [(int)file.length()];
+                        fileinputstream = new FileInputStream(file);
+                        bufferinputstream = new BufferedInputStream(fileinputstream);
+                        bufferinputstream.read(byteArray,0,byteArray.length);
+                        outputstream = soc.getOutputStream();
+                        Handler h = new Handler(Looper.getMainLooper());
+
+                        h.post(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Toast.makeText(Upload_Service.this.getApplicationContext(),"debug",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        outputstream.write(byteArray,0,byteArray.length);
+                        outputstream.flush();
+                    }
+                    finally {
+                        if (bufferinputstream != null) bufferinputstream.close();
+                        if (outputstream != null) outputstream.close();
+                        if (soc!=null) soc.close();
+                    }
+
+                }
+                writer.close();
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
+/*Handler handler = new Handler(Looper.getMainLooper());
+
+handler.poutputstreamt(new Runnable() {
+
+@Override
+public void run() {
+        Toast.makeText(Upload_Service.this.getApplicationContext(), "debug",Toast.LENGTH_SHORT).show();
+        }
+        });*/
